f38f2e4c418a65627bd1da1e2ca2f91154625441
Rafael Badaro <rafabadaro.15@gmail.com>
2022-06-14 16:44:48 +0200

    Adding sass

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const sass = require('gulp-dart-sass');
+const prefix = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const gcmqp = require('gulp-css-mqpacker');
+
+// Compile sass into CSS (/src/css/)
+gulp.task('sass', () =>
+    gulp
+        .src('./scss/style.scss')
+        .pipe(
+            plumber({
+                errorHandler: notify.onError({
+                    title: 'SASS compile error!',
+                    message: '<%= error.message %>',
+                }),
+            })
+        )
+        .pipe(sourcemaps.init())
+        // outputStyle: expanded or compressed
+        .pipe(sass.sync({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(prefix('last 2 versions'))
+        .pipe(gcmqp())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+);
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        startPath: 'index.html',
+        port: 7777,
+
+        ui: {
+            port: 7779,
+        },
+    });
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+    gulp.watch('./src/**/*.{html,css,js}').on('change', browserSync.reload);
+});
+
+gulp.task('build', gulp.series('sass'));
+gulp.task('default', gulp.series('sass', 'browser-sync'));

e5c80146250fe0eca1c58b1fbc851fcd7de2e21b
Rafael Badaro <rafabadaro.15@gmail.com>
2025-02-05 22:28:18 -0300

    Adding just a black background with text + links to linkedin and github

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -4,26 +4,46 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <h1>Software engineer</h1>
+
+        <div>
+          <h2>
+            Hi! Im Rafael, a Software engineer with passion for mobile and web dev
+          </h2>
+          <h2>
+            Currently working as an iOS engineer at Leal Apps, building fitness and health apps
+          </h2>
+        </div>
+
+        {/* <h2>Check out my Projects, Linkedin and Github</h2> */}
+
+        <div className="row-start-3 flex gap-2 flex-wrap items-center justify-center">
+          <span> Check out my </span>
+          <a
+            className="flex items-center gap-2 underline text-blue-600 hover:text-blue-800 visited:text-purple-600 hover:underline hover:underline-offset-4"
+            href="https://www.linkedin.com/in/rafael-badaro/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+
+          <span>and</span>
+
+          <a
+            className="flex items-center gap-2 text-gray-700 hover:underline hover:underline-offset-4"
+            href="https://github.com/RafaelBadaro"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github
+          </a>
+        </div>
+
+
+
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -47,9 +67,10 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
+
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -95,7 +116,7 @@ export default function Home() {
           />
           Go to nextjs.org →
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 }
